31b6e83b10bda7fb8ce61bccb8d29c5959d585e6
Rune Moskvil Lyngås <runely@users.noreply.github.com>
2025-11-21 09:18:44 +0100

    patch: 1.0.1 - trusted-publishing (#1)
    * chore: Use Trusted Publishing instead of long-lived API keys
    
    * chore: bump version to 1.0.1
    
    * chore: Use Trusted Publishing instead of long-lived API keys
    
    * chore: Turn off ImplicitUsings
    
    * fix: Tests needs to be built before running

--- Vestfold.Extensions.MongoDb/Services/IMongoDbService.cs
+++ Vestfold.Extensions.MongoDb/Services/IMongoDbService.cs
@@ -1,3 +1,4 @@
+using System.Threading.Tasks;
 using MongoDB.Driver;
 
 namespace Vestfold.Extensions.MongoDb.Services;
--- Vestfold.Extensions.MongoDb/Services/MongoDbService.cs
+++ Vestfold.Extensions.MongoDb/Services/MongoDbService.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Threading.Tasks;
 using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
